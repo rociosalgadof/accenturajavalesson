f81b29945beda9a8fdcf2099c5d59919cbbf780d
Rocio Salgado <salgadorocio@hotmail.com>
2023-04-11 09:52:54 +0200

    creates packages

--- src/com/ironhack/w1/d1/Main.java
+++ src/com/ironhack/w1/d1/Main.java
@@ -1,3 +1,5 @@
+package com.ironhack.w1.d1;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
